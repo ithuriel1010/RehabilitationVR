59701fdfc29566cb4a343807a7083b09b6934e5d
Aleksandra Wędołowska <ithuriel1010@gmail.com>
2022-06-13 20:21:04 +0200

    It's possible to move from menu to the result of level 1

--- Assets/Scripts/BodyCollision.cs
+++ Assets/Scripts/BodyCollision.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 using Mono.Data.Sqlite;
 using System.IO;
 using System;
+using UnityEngine.SceneManagement;
 
 public class BodyCollision : MonoBehaviour
 {
     public AudioSource audioSource;
-    private string dbName = "URI=file:GameLog.db";
+    private string dbName = "URI=file:GameLog6.db";
     private int hits = 0;
+    private float startTime;
+
+    bool keepTiming;
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +27,47 @@ public class BodyCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (keepTiming)
+        {
+            UpdateTime();
+        }
     }
 
+    void UpdateTime()
+    {
+        timer = Time.time - startTime;
+        //Debug.Log(timer);
+    }
+
+    void StartTimer()
+    {
+        keepTiming = true;
+        startTime = Time.time;
+    }
+
+    void StopTimer()
+    {
+        Debug.Log("Level finished");
+
+        keepTiming = false;
+
+        UpdateDatabase();
+        SceneManager.LoadScene(2);
+    }
+
+    /*IEnumerator time()
+    {
+        while (true)
+        {
+            timeCount();
+            yield return new WaitForSeconds(1);
+        }
+    }
+    void timeCount()
+    {
+        timer += 1;
+        Debug.Log("Seconds:" + timer);
+    }*/
     private void CreateDB()
     {
         using(var connection = new SqliteConnection(dbName))
@@ -33,7 +76,7 @@ public class BodyCollision : MonoBehaviour
 
             using(var command = connection.CreateCommand())
             {
-                command.CommandText = "CREATE TABLE IF NOT EXISTS levelOne (hits INT);";
+                command.CommandText = "CREATE TABLE IF NOT EXISTS levelOne (hits INT, time INT);";
                 command.ExecuteNonQuery();
             }
 
@@ -44,7 +87,46 @@ public class BodyCollision : MonoBehaviour
     private void ReadDB()
     {
         int points=0;
+        double time = 0;
         using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT MIN(time) FROM levelOne;";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log(reader[0]);
+                        //points = int.Parse(reader[0].ToString());
+                        time = (double)reader[0];
+                    }
+
+                    reader.Close();
+                }
+
+                command.CommandText = "SELECT MIN(hits) FROM levelOne;";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log(reader[0]);
+                        points = int.Parse(reader[0].ToString());
+                        //time = (double)reader[0];
+                    }
+
+                    reader.Close();
+                }
+            }
+
+            connection.Close();
+        }
+
+        /*using (var connection = new SqliteConnection(dbName))
         {
             connection.Open();
 
@@ -58,17 +140,18 @@ public class BodyCollision : MonoBehaviour
                     {
                         Debug.Log(reader[0]);
                         points = int.Parse(reader[0].ToString());
+                        //time = (double)reader[0];
                     }
 
                     reader.Close();
                 }
-
             }
 
             connection.Close();
-        }
+        }*/
 
-        Debug.Log("Max hits:" + points);
+        Debug.Log("Min time:" + time);
+        Debug.Log("Max points:" + points);
     }
 
 
@@ -79,7 +162,7 @@ public class BodyCollision : MonoBehaviour
         hits += 1;
     }
 
-    private void OnApplicationQuit()
+   /* private void OnApplicationQuit()
     {
         using (var connection = new SqliteConnection(dbName))
         {
@@ -87,7 +170,24 @@ public class BodyCollision : MonoBehaviour
 
             using (var command = connection.CreateCommand())
             {
-                command.CommandText = "INSERT INTO levelOne(hits) VALUES ("+ hits + ");";
+                command.CommandText = "INSERT INTO levelOne(hits, time) VALUES (" + hits + ", " + timer +  ");";
+                command.ExecuteNonQuery();
+            }
+
+            connection.Close();
+        }
+    }*/
+
+    private void UpdateDatabase()
+    {
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                //Debug.Log("INSERT INTO levelOne(hits, time) VALUES (" + hits + ", " + timer + ");");
+                command.CommandText = "INSERT INTO levelOne(hits, time) VALUES (" + hits + ", " + timer + ");";
                 command.ExecuteNonQuery();
             }
 
--- Assets/Scripts/MenuOptions.cs
+++ Assets/Scripts/MenuOptions.cs
@@ -1,14 +1,128 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using Mono.Data.Sqlite;
+using System.IO;
+using System;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MenuOptions : MonoBehaviour
 {
+    private string dbName = "URI=file:GameLog6.db";
+
+    public Text scoreGame1;
+    int points = 0;
+    double time = 0;
+    int lastPoints = 0;
+    int lastTime = 0;
+
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        var scene = SceneManager.GetActiveScene().buildIndex;
+
+        if(scene ==2)
+        {
+            ReadDB();
+            GetCurrentScore();
+            scoreGame1.text = $"Zakończyłeś \n poziom pierwszy \n\n Twój wynik: \n\n Czas: {lastTime} \n Uderzenia: {lastPoints}\n Najlepszy czas: {time}\n Najmniej uderzeń: {points}";
+        }
+    }
+
+    private void ReadDB()
+    {
+        
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT MIN(time) FROM levelOne;";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        //Debug.Log(reader[0]);
+                        //points = int.Parse(reader[0].ToString());
+                        time = (double)reader[0];
+                    }
+
+                    reader.Close();
+                }
+
+                command.CommandText = "SELECT MIN(hits) FROM levelOne;";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        //Debug.Log(reader[0]);
+                        points = int.Parse(reader[0].ToString());
+                        //time = (double)reader[0];
+                    }
+
+                    reader.Close();
+                }
+            }
+
+            connection.Close();
+        }
+    }
+
+    private void GetCurrentScore()
+    {
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT time FROM levelOne WHERE rowid = (SELECT MAX(rowid)  FROM levelOne); ";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log(reader[0]);
+                        //points = int.Parse(reader[0].ToString());
+                        lastTime = (int)reader[0];
+                    }
+
+                    reader.Close();
+                }
+
+                command.CommandText = "SELECT hits FROM levelOne WHERE rowid = (SELECT MAX(rowid)  FROM levelOne); ";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log(reader[0]);
+                        //points = int.Parse(reader[0].ToString());
+                        lastPoints = (int)reader[0];
+                    }
+
+                    reader.Close();
+                }
+            }
+
+            connection.Close();
+        }
+    }
     public void ChangeScene()
     {
         Debug.Log("GUZIK!!!!");
         SceneManager.LoadScene(1);
+    }
+
+    public void GoToGame2()
+    {
+        SceneManager.LoadScene(3);
     }
 }
